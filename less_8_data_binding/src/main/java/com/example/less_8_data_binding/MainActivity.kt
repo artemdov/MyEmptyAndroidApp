b28796f08cff7f6453ebd03f0fce6ae4f59cec3f
user <artemdovger26@gmail.com>
2025-01-31 14:15:40 +0300

    less 9, data binding with observers and observable data fo UI

--- less_8_data_binding/src/main/java/com/example/less_8_data_binding/MainActivity.kt
+++ less_8_data_binding/src/main/java/com/example/less_8_data_binding/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.less_8_data_binding
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.less_8_data_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by lazy { ViewModelProvider(this)[SimpleViewModel::class.java] }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,8 +17,8 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.name = "Adam"
-        binding.lastName = "Sandler"
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
 
     }
 }
--- less_8_data_binding/src/main/java/com/example/less_8_data_binding/SimpleViewModel.kt
+++ less_8_data_binding/src/main/java/com/example/less_8_data_binding/SimpleViewModel.kt
@@ -1,20 +1,31 @@
 package com.example.less_8_data_binding
 
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
 class SimpleViewModel : ViewModel() {
-    val name = "Garry"
-    val lastName = "Hopper"
-    private var likes = 0
+    private val _name = MutableLiveData("Tom")
+    private val _lastName = MutableLiveData("Soyer")
+    private val _likes = MutableLiveData(0)
+
+    val name: LiveData<String> = _name
+    val lastName: LiveData<String> = _lastName
+    var likes: MutableLiveData<Int> = _likes
+
 
     fun onLike() {
-        likes++
+        _likes.value = (_likes.value ?: 0) + 1
     }
 
-    fun popularity() = when {
-        likes > 9 -> Popularity.STAR
-        likes > 4 -> Popularity.POPULAR
-        else -> Popularity.NORMAL
+    fun popularity(): LiveData<Popularity> = _likes.map {
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
+        }
     }
 }
 

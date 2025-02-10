2a68510d798c474918f8effd6922837d5ef637b0
user <artemdovger26@gmail.com>
2025-02-10 12:49:11 +0300

    added coroutines

--- coroutines/src/main/java/com/example/coroutines/MainActivity.kt
+++ coroutines/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.coroutines
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutines.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -22,64 +23,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonLoad.setOnClickListener {
-            loadData()
+            lifecycleScope.launch {
+                loadData()
+            }
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun loadData() {
+    private suspend fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
 
-//       меняем код синхронный на асинхронный ниже
-//        val city = loadCity()
-//        binding.tvLocation.text = city
-//        val temp = loadTemperature(city)
-//        binding.tvTemperature.text = temp.toString()
-//        binding.progress.isVisible = false
-//        binding.progress.isEnabled = true
 
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
 
-        //выполнение уже будет в новом потоке
-        loadCity { its ->
-            binding.tvLocation.text = its
-            loadTemperature(its) {
-                binding.tvTemperature.text = it.toString()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-            }
-        }
     }
 
-    private fun loadCity(callback: (String) -> Unit) {
 
-        //создание нового потока
-        thread {
-            Thread.sleep(5000)
-            runOnUiThread {
-                callback.invoke(
-                    "Moscow"
-                )
-            }
-        }
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Moscow"
+
+
     }
 
-    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this, "Loading temp for city: $city",
+            Toast.LENGTH_SHORT
+        ).show()
 
-        thread {
-            runOnUiThread {
-                Toast.makeText(
-                    this, "Loading temp for city: $city",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+        delay(5000)
 
-            Thread.sleep(5000)
-
-            runOnUiThread {
-                callback.invoke(17)
-            }
-        }
-
+        return 17
     }
 }
